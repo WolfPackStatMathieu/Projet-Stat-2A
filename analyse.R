eac93d8208e8573cbfea10bad861b73636e04cc5
Cyril Nebanga <cnebanga@gmail.com>
2023-01-22 19:03:24 +0100

    code

--- analyse.R
+++ analyse.R
@@ -10,6 +10,17 @@ N <- 18
 
 df <- cbind.data.frame(dose, reponse)
 
-crm(prior = dosesWm, target = cibleP, tox = reponse, level = dose, n = N)
+crm(prior = dosesWm, 
+    target = cibleP,
+    tox = reponse,
+    level = dose,
+    n = N,
+    model = "empiric")
 
-?crm()
+# On remarque que la probabilité de toxicité augmente au fur et à mésure que
+# le niveau de dose augmente.Et que la prochaine dose recommandée est de 5.
+# La variance postérieure de bêta étant faible indique que les données 
+# sont informatives sur le paramètre bêta. Ce qui semble suggérer que le
+# modèle est adapté aux données.
+
+
