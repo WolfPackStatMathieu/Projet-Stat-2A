b7c3a7d463521b102cd96e952867469a7e67f422
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-02-12 20:02:02 +0100

    on vire beta0

--- glm.R
+++ glm.R
@@ -14,9 +14,9 @@ fonction_logit<-function(beta0=0,beta1,x){
   (1+exp(beta1*x+beta0))
   return(exp(beta1*x+beta0)/(1+exp(beta1*x+beta0)))
 }
-beta0<-logit_proba_dlt$coefficients[["(Intercept)"]]
+
 beta1<-logit_proba_dlt$coefficients[["xref[donnees$dose]"]]
-y_predicted<-sapply(xref[c(1:5)],fonction_logit,beta0=beta0,beta1=beta1)
+y_predicted<-sapply(xref[c(1:5)],fonction_logit,beta0=0,beta1=beta1)
 plot(x=c(1:5),y=y_predicted,xlab="Index de la dose",ylab="Valeur de la probabilité")
 test_bayes<-modele_survie_bayes(p,tstar,observations_time,id_dose,valeur_dose =xref,vecteur_reponse = vecteur_reponse )
 afficher_resultat(beta=test_bayes,x_ref,skeleton)
