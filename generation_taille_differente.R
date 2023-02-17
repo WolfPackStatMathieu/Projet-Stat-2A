55e9ecac9e2bf600deae05de93c97d0e1cd3be81
GORSE Nathan <id2021@DOMENSAI.ECOLE>
2023-02-17 09:41:33 +0100

    work on graphs

--- generation_taille_differente.R
+++ generation_taille_differente.R
@@ -5,7 +5,7 @@ fonction_generation_taille_differente<-function(vector_size,model,liste_paramete
   ### renvoie la génération avec des tailles différentes du modèle model (string) ayant comme paramètre la liste_parameter, 
   ### liste de paramètres avec le modèles. 1 seul comme bernoulli et 2 pour exp() (lambda et t_star).
   if (model=="bernoulli"){
-    vecteur_realisation<-sapply(vector_size,biais_pi,liste_parameter[["p"]])
+    vecteur_realisation<-sapply(vector_size[order(vector_size)],biais_pi,liste_parameter[["p"]])
     return(vecteur_realisation)
   }
   else{if(model=="surv"){
@@ -28,3 +28,28 @@ list_param<-list(prop)
 names(list_param)<-c("p")
 modele2<-"bernoulli"
 test_generation_taillediff_bern<-fonction_generation_taille_differente(vector_size = vecteur_size,model=modele2,liste_parameter = list_param)
+
+fonction_graph_fonc_size<-function(vector_size,model,liste_parameter){
+  vector_size<-vector_size[order(vector_size)]
+  results_vector<-fonction_generation_taille_differente(vector_size = vector_size,model=model,liste_parameter = liste_parameter)
+  bias_size_data<-cbind.data.frame(vector_size,results_vector)
+  colnames(bias_size_data)<-c("Size","Bias")
+  return(bias_size_data)
+}
+
+############# TEST graph ###########
+
+######1) exp#####
+N<-50
+vecteur_size<-sample(c(10:1000),N)
+lamdba_test<-0.33
+t_star<-6
+liste_parameter<-list(lambda_test,t_star)
+names(liste_parameter)<-c("lambda","t_star")
+modele<-"surv"
+test_graph_exp<-fonction_graph_fonc_size(vector_size = vecteur_size,model=modele,liste_parameter=liste_parameter)
+h_DPI<-KernSmooth::dpill(x=test_graph_exp$Size,y=test_graph_exp$Bias)
+estimation_y<-KernSmooth::locpoly(x=test_graph_exp$Size,y=test_graph_exp$Bias,bandwidth=h_DPI,degree=3,gridsize = N)$y
+
+plot(test_graph_exp,col="blue",main="Value of the bias in surv model according to the size",type="p")
+lines(estimation_y)
