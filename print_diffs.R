d6bdd863088e7300d9937e9a734e126427341f96
Nathan GORSE <gorsenath@gmail.com>
2023-02-18 17:41:27 +0100

    graph

--- print_diffs.R
+++ print_diffs.R
@@ -2,9 +2,12 @@
 source("generation_mean.R")
 set.seed(133)
 
-fonction_compar_plots<-function(vector_size,p,lambda,t_star,K){
+fonction_compar_plots<-function(limit_inf,limit_sup,N,p,lambda,t_star,K){
+  #### N corresponds to the number of sizes. K correspond to the number of samples for each size. 
   require(gridExtra)
   require(ggplot2)
+  vector_size<-sample(c(limit_inf:limit_sup),N)
+  vector_size<-vector_size[order(vector_size)]
   liste_param1<-list(p)
   names(liste_param1)<-c("p")
   modele_bern<-"bernoulli"
@@ -13,7 +16,7 @@ fonction_compar_plots<-function(vector_size,p,lambda,t_star,K){
   names(liste_param2)<-c("lambda","t_star")
   modele_exp<-"surv"
   result2<-fonction_generation_taille_mean(vector_size,modele_exp,liste_param2,K)
-  whole_data_expbern<-cbind.data.frame(vecteur_size[order(vecteur_size)],result1,result2)
+  whole_data_expbern<-cbind.data.frame(vector_size,result1,result2)
   colnames(whole_data_expbern)<-c("Size","Mean_Bias_Bern","Mean_Bias_Surv")
   ####plot 
   gg1<-ggplot(data=whole_data_expbern,aes(x=Size,y=Mean_Bias_Bern))+
@@ -31,4 +34,7 @@ p2<-0.33
 k<-20
 lambda7<-0.33
 t_star<-6
-test_plot<-fonction_compar_plots(vector_size =vecteur_size,p=p2,lambda=lambda7,t_star=t_star,K=k)
+lmoins<-1
+l_plus<-1000
+N<-20
+test_plot<-fonction_compar_plots(limit_sup = l_plus,limit_inf = lmoins,N=N,p=p2,lambda=lambda7,t_star=t_star,K=k)
