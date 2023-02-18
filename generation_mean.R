4034fefa88457b54099f201aa59fbd96d0a42236
Nathan GORSE <gorsenath@gmail.com>
2023-02-18 16:12:44 +0100

    Use of Nsimulations per size

---
+++ generation_mean.R
@@ -0,0 +1,36 @@
+
+source("surv.R")
+source("bernoulli.R")
+fonction_generation_taille_mean<-function(vector_size,model,liste_parameter,K){
+  ### renvoie la génération avec des tailles différentes du modèle model (string) ayant comme paramètre la liste_parameter, 
+  ### liste de paramètres avec le modèles. 1 seul comme bernoulli et 2 pour exp() (lambda et t_star).
+  vector_size<-vector_size[order(vector_size)]
+  if (model=="bernoulli"){
+    vecteur_realisation<-sapply(vector_size,Simuler_Nfois_n_echantillons_bern,N=K,liste_parameter[["p"]])
+    return(colMeans(vecteur_realisation))
+  }
+  else{if(model=="surv"){
+    vecteur_realisation<-sapply(vector_size,Simuler_Nfois_n_echantillons,N=K,lambda=liste_parameter[["lambda"]],t_star=liste_parameter[["t_star"]])
+    return(colMeans(vecteur_realisation))
+  }}
+}
+
+#################" TEST exp de la méthode.#####
+N<-100
+vecteur_size<-sample(c(1:1000),N)
+lamdba_test<-0.33
+t_star<-6
+liste_parameter<-list(lambda_test,t_star)
+names(liste_parameter)<-c("lambda","t_star")
+modele<-"surv"
+K<-50
+test_generation_taillemoy<-fonction_generation_taille_mean(vector_size=vecteur_size,model=modele,K=k,liste_parameter = liste_parameter)
+
+
+#################### Plot des résultats en fonction de la taille.########
+donnees_taille_biaismoyen<-cbind.data.frame(vecteur_size[order(vecteur_size)],test_generation_taillemoy)
+colnames(donnees_taille_biaismoyen)<-c("Size","Mean_Bias")
+h_DPImean<-KernSmooth::dpill(x=donnees_taille_biaismoyen$Size,y=donnees_taille_biaismoyen$Mean_Bias)
+estimation_ymoy<-KernSmooth::locpoly(x=donnees_taille_biaismoyen$Size,y=donnees_taille_biaismoyen$Mean_Bias,bandwidth=h_DPImean,degree=3,gridsize = N)$y
+plot(donnees_taille_biaismoyen)
+lines(x=donnees_taille_biaismoyen$Size,y=estimation_ymoy,col="red")
