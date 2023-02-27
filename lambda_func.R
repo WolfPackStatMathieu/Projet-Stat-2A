f77a782d48191d5858bda9bbd1d59b63f8d4c490
Nathan GORSE <gorsenath@gmail.com>
2023-02-27 15:07:25 +0100

    modification

--- lambda_func.R
+++ lambda_func.R
@@ -3,25 +3,27 @@ source("surv.R")
 source("bernoulli.R")
 library("ggplot2")
 
-lambda_func <- function(lambda_vec, list_params){
-  vec_biais_surv <- sapply(lambda_vec,fonction_biais_survie,n=list_params[["n"]],
-                           t_star=list_params[["t_star"]])
-  return(vec_biais_surv)
+lambda_func <- function(lambda_vec, list_params,N){
+  vec_biais_surv <- cbind.data.frame(sapply(lambda_vec,Simuler_Nfois_n_echantillons,n=list_params[["n"]],
+                           t_star=list_params[["t_star"]],N=N))
+  return(colMeans(vec_biais_surv))
 }
 
-pbinom_func <- function(p_vec, n){
-  vec_biais_binom <- sapply(p_vec,biais_pi,n=n)
-  return(vec_biais_binom)
+pbinom_func <- function(p_vec, n,N){
+  vec_biais_binom <- cbind.data.frame(sapply(p_vec,Simuler_Nfois_n_echantillons_bern,n=n,N=N))
+  return(colMeans(vec_biais_binom))
 }
 
 
 # test binom
-vec_p <- c(0.2,0.33,0.24,0.12,0.18)
-test1 <- pbinom_func(vec_p, n)
+vec_p <- seq.int(from=0.1,to=0.9,by=0.07)
+N<-100
+n<-100
+test1 <- pbinom_func(vec_p, n,N)
 
 
 ggplot(data.frame(x = vec_p, y = test1), aes(x = x, y = y)) +
-  geom_line(color = "blue", size = 1.5) +
+  geom_line(color = "Sky blue", size = 1.5) +
   ggtitle("Bias with different p") +
   xlab("p") + ylab("Biais") +
   theme_minimal()
@@ -30,12 +32,14 @@ test1
 
 
 # test surv
+N<-100
 n<-100
 t_star<-6
 lambda_vec <- c(1.8,1.41, 1.28, 1.25,1.42, 1.04)
+lambda_vec<-lambda_vec[order(lambda_vec)]
 liste_parameter<-list(n,t_star)
 names(liste_parameter)<-c("n","t_star")
-test <-lambda_func(lambda_vec=lambda_vec,list_params =  liste_parameter)
+test <-lambda_func(lambda_vec=lambda_vec,list_params =  liste_parameter,N)
 ggplot(data.frame(x = lambda_vec, y = test), aes(x = x, y = y)) +
   geom_line(color = "blue", size = 1.5) +
   ggtitle("Bias with different lambdas") +
--- print_diffs.R
+++ print_diffs.R
@@ -19,11 +19,11 @@ fonction_compar_plots<-function(limit_inf,limit_sup,N,p,lambda,t_star,K){
   colnames(whole_data_expbern)<-c("Size","Mean_Bias_Bern","Mean_Bias_Surv")
   ####plot 
   gg1<-ggplot(data=whole_data_expbern,aes(x=Size,y=Mean_Bias_Bern))+
-    geom_smooth(colour="red")+
+    geom_point(colour="red")+
     labs(y="Mean Bias with Bern model")
   
   gg2<-ggplot(data=whole_data_expbern,aes(x=Size,y=Mean_Bias_Surv))+
-    geom_smooth(colour="blue")+
+    geom_point(colour="blue")+
     labs(y="Mean Bias with Surv model")
   
   whole_g<-grid.arrange(gg1,gg2,ncol=2,top="Comparison of the two methods")
@@ -38,3 +38,78 @@ lmoins<-1
 l_plus<-1000
 N<-50
 test_plot<-fonction_compar_plots(limit_sup = l_plus,limit_inf = lmoins,N=N,p=p2,lambda=lambda7,t_star=t_star,K=k)
+NSimulations.selon.n<-function(N,lambda,t_star){
+  #' Matrice composee des biais moyens associes a la taille de l'echantillon de n=20 a n=200 par saut de 20.
+  #'
+  #' @param N nombre de tailles d'echantillon differents.
+  #' @param lambda parametre de la loi exponentielle.
+  #' @param t_star fin de la fenetre d'observation
+  #'
+  #' @return Valeur du biais moyen selon n dans l'intervalle (20,200).
+  #' @export
+  #'
+  #' @examples
+  #' ######Test ######
+  #' t_star<-3
+  #' N<-10
+  #' lambda<-c(0.2)
+  #' result<-NSimulations.selon.n(N,lambda,t_star)
+  
+  results<- NULL
+  n<- 20
+  while (n<200)
+  {
+    vecteur_biais<-rep(NA,N)
+    biais<-  Simuler_Nfois_n_echantillons(N,n,lambda,t_star)
+    results<-rbind(results,c(n,mean(biais)  ))
+    n<- n+20
+  }
+  return(results)
+}
+fonction_compar_plotsn_lambda<-function(N,window_lambda,t_star){
+  #' Plot des valeurs des biais moyens selon la taille des echantillons et du lambda.
+  #'
+  #' @param N nombre de tailles d'echantillon differents.
+  #' @param window_lambda
+  #' @param t_star fin de la fenetre d'observation
+  #'
+  #' @return Plot des valeurs des biais moyens en fonction du lambda et de la taille des echantillons.
+  #' @export
+  #'
+  #' @examples
+  #' ######Test ######
+  #' t_star<-3
+  #' N<-10
+  #' window_lambda<-c(0.2,0.5,0.1)
+  #' result<-fonction_compar_plotsn_lambda(N,window_lambda,t_star)
+  
+  set.seed(12345)
+  RES<- NULL
+  RES<- NSimulations.selon.n(N,window_lambda[1],t_star)
+  RES0.2.3<-data.frame(RES)
+  colnames( RES0.2.3)<- c("n","mean.bias")
+  set.seed(12345)
+  RES<- NULL
+  RES<- NSimulations.selon.n(N,window_lambda[2],t_star)
+  RES0.5.3<-data.frame(RES)
+  colnames( RES0.5.3)<- c("n","mean.bias")
+  
+  set.seed(12345)
+  RES<- NULL
+  RES<- NSimulations.selon.n(N,window_lambda[3],t_star)
+  RES0.1.3<-data.frame(RES)
+  colnames( RES0.1.3)<- c("n","mean.bias")
+  
+  plot(RES0.2.3$n,RES0.2.3$mean.bias,title=paste("Influence of n"),
+       ylim=c(-0.05,0.05),type='b',bty="n",xlab="nbre sujets",ylab="biais moyen")
+  title("Influence de n et lambda")
+  lines(RES0.5.3$n,RES0.5.3$mean.bias,type="b",col="blue")
+  lines(RES0.1.3$n,RES0.1.3$mean.bias,type="b",col="red")
+  abline(h=0)
+  legend("topright",c("0.1","0.2","0.5"),col=c("red","black","blue"),lty=1,bty="n")
+}
+
+window_lambda<-c(0.2,0.5,0.1)
+N<-50
+t_star<-6
+test_compar_lambda<-fonction_compar_plotsn_lambda(N,window_lambda,t_star)
