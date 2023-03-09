858d998fad9a77582054afa0dd2f6434d38860a6
Nathan GORSE <gorsenath@gmail.com>
2023-03-09 10:09:17 +0100

    choix k

---
+++ choix_k.R
@@ -0,0 +1,20 @@
+function_choice_k<-function(Coverage,target_MCerror){
+  return(Coverage*(100-Coverage)/(target_MCerror)^2)
+}
+function_ergodic_mean<-function(n,p){
+  values_pi<-rbern(n,p)
+  values_mean_ergodique<-cumsum(values_pi)/(1:n)
+  return(values_mean_ergodique)
+}
+function_print_p_theoretical<-function(n,p){
+  values_ergodic<-function_ergodic_mean(n,p)
+  data_size_ergodic<-cbind.data.frame(values_ergodic,c(1:n))
+  colnames(data_size_ergodic)<-c("Ergodic","Size")
+  result<-{plot(x=data_size_ergodic$Size,y=data_size_ergodic$Ergodic,
+                main="Value of the ergodic mean",type="l",xlab="Size",ylab="Ergodic mean")
+  abline(h=p,col="blue")}
+  return(result)
+}
+n<-10000
+p<-0.33
+result_print<-function_print_p_theoretical(n,p)
