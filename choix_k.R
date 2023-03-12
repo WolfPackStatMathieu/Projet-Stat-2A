69579f385718986ac37d1c69839258fed7da791b
Nathan GORSE <gorsenath@gmail.com>
2023-03-12 11:34:52 +0100

    travail calcul nsim

--- choix_k.R
+++ choix_k.R
@@ -1,3 +1,5 @@
+source("surv.R")
+source("bernoulli.R")
 function_choice_k<-function(Coverage,target_MCerror){
   return(Coverage*(100-Coverage)/(target_MCerror)^2)
 }
@@ -18,3 +20,27 @@ function_print_p_theoretical<-function(n,p){
 n<-10000
 p<-0.33
 result_print<-function_print_p_theoretical(n,p)
+### le but est de calculer l'erreur de Monte Carlo du "coverage" soit de la 
+### probabilite que theta soit present dans l'intervalle entre theta-inf et 
+### theta-sup. 
+### On somme ces probabilites pour chaque echantillon creee. 
+### creation des estimateurs. 
+function_calc_MCSE<-function(n,p,nsim,coverage){
+  somme_proba<-0
+  alpha<-1-coverage
+  for(i in c(1:nsim)){
+  result<-simul_bernoulli(n,p)
+  estim_moins<-mean(result)-qnorm(1-(alpha/2))*var(result)
+  estim_plus<-mean(result)+qnorm(1-(alpha/2))*var(result)
+  reponse=ifelse((estim_plus>p && p>estim_moins)==TRUE,1,0)
+  somme_proba<-somme_proba + reponse
+  }
+  coverage_estimated<-somme_proba/(nsim)
+  MCSE<-sqrt(coverage_estimated*(1-coverage_estimated)/nsim)
+  return(MCSE)
+}
+n<-5
+nsim<-4
+p<-0.33
+coverage<-0.95
+test_MCerror<-function_calc_MCSE(n,p,nsim,coverage)
