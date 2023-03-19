2bea3abc1afbb972b1d855369a4217ca1bb7c70d
Cyril Nebanga <cnebanga@gmail.com>
2023-03-19 15:18:04 +0100

    scenario 1

---
+++ scenario_1.R
@@ -0,0 +1,9 @@
+################################ Scénario 1 ################################################
+
+rm(list = ls())
+source("surv.R")
+
+n <- 10
+lambda <- 
+
+biais.2.estim <- Simuler_biais_un_n_ech()
