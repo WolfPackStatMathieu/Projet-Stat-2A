4aa0a2e36eec9dd391ae002fdb159fe978289dd2
Nathan GORSE <gorsenath@gmail.com>
2023-03-20 22:00:57 +0100

    travail sur cure

--- estimateurs/estimateur_cure.R
+++ estimateurs/estimateur_cure.R
@@ -9,18 +9,26 @@ fonction_cure<-function(df,t_star){
   surv_object<-Surv(df$tox_time,event=df$is_observed)
   ## group correspond au groupe des personnes à risque ou non. 
   df$sensible<-as.numeric(df$sensible)
+  probabilite_etre_sensible<-mean(df$sensible)
+  df<-df[,c("tox_time","is_observed")]
   result<-flexsurvcure(surv_object ~1, data = df, link="logistic", dist="weibullPH", mixture=T)
   shape_model<-result[["coefficients"]][["shape"]]
-  probabilite_etre_sensible<-mean(df$sensible)
-  probabilite_survenue_sensible<-result$dfns$p(t_star,theta=probabilite_etre_sensible,shape=shape_model)
+  scale<-result[["coefficients"]][["scale"]]
+  ### la valeur de scale peut être négative. Le scale correspond aux covariables. 
+  ## On retrouve le lambda en calculant exp(z(t)*beta)
+  probabilite_survenue_sensible<-exp(-exp(scale)*(t_star)^(shape))
   #1 modele de melange#
   # pour les sensibles#
   probabilitegroup1<-probabilite_etre_sensible*probabilite_survenue_sensible
-  return(probabilite_survenue_sensible)
+  probabilitegroup2<-1-probabilite_etre_sensible
+  probabilite_DLT<-1-(probabilitegroup1+probabilitegroup2)
+  return(probabilite_DLT)
 }
 
 ###### Test###
 df<-Generation_un_ech(n=10,lambda=0.1,p=0.5,k=1,t_star=6)
-str(df)
 appel_cure<-fonction_cure(df,t_star=6)
+print(appel_cure)
 mean(df$is_observed)
+flexsurvcure(Surv(rectime,censrec)~group, data=bc, dist="weibull", anc=list(scale=~group))
+print(bc)
