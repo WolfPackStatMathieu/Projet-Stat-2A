c36a4eb8c0b48aecec33d193a31edc030681e9fa
Cyril Nebanga <cnebanga@gmail.com>
2023-03-27 11:19:05 +0200

    conflit

--- scenario_1.R
+++ scenario_1.R
@@ -137,11 +137,8 @@ biais.selon.taille_echantillon <- function(K, lambda, t_star, p, k){
 
 plots_scenario_1(K=1900, n=100, lambda=0.5, t_star=6, p=0.3, k=1)
 
-<<<<<<< HEAD
-biais.selon.taille_echantillon(K = 10, lambda = 0.5, t_star = 6, p = 0.3, k=1)
-=======
-biais.selon.taille_echantillon(K = 1900, lambda = 0.5, t_star = 6, p = 0.3, k=2)
->>>>>>> 18f853f4260f0c7d11960807b87d771a1bd2e7f4
+biais.selon.taille_echantillon(K = 1900, lambda = 0.5, t_star = 6, p = 0.3, k=1)
+
 
 
 
