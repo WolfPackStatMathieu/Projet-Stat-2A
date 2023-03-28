6b88cc4680a5a843fda8a95ec7f230e609c937ce
Cyril Nebanga <cnebanga@gmail.com>
2023-03-28 21:36:56 +0200

    graphiques selon k tout bon

--- selon.k.R
+++ selon.k.R
@@ -1,5 +1,4 @@
 source("surv.R")
-source("bernoulli.R")
 source("weibull.R")
 
 
@@ -52,8 +51,6 @@ fnct_compar_plt_biais.selon.k<-function(N, n, window_lambda,t_star,p){
   RES0.1.3<-data.frame(RES)
   colnames( RES0.1.3)<- c("k","mean.surv", "mean.cure", "mean.bernoulli")
   
-  list_plot <- list(c(1:3))
-  
   
   borne_min<-min(min(RES0.5.3$mean.surv),min(RES0.1.3$mean.surv),min(RES0.2.3$mean.surv))
   borne_max<-max(max(RES0.5.3$mean.surv),max(RES0.1.3$mean.surv),max(RES0.5.3$mean.surv))
@@ -72,97 +69,138 @@ fnct_compar_plt_biais.selon.k<-function(N, n, window_lambda,t_star,p){
   lines(RES0.1.3$k,RES0.1.3$mean.surv,type="b",col="red")
   abline(h=0)
   
-  
+
   plot(RES0.2.3$k,RES0.2.3$mean.cure, main = "Modèle de guérison",
             ylim=c(-0.1+borne_min.c,borne_max.c+0.1),
             type='b',xlab=expression(alpha),ylab="biais moyen")
   lines(RES0.5.3$k,RES0.5.3$mean.cure,type="b",col="blue")
   lines(RES0.1.3$k,RES0.1.3$mean.cure,type="b",col="red")
   abline(h=0)
-  
+
   plot(RES0.2.3$k,RES0.2.3$mean.bernoulli,main="Modèle de Bernoulli",
             ylim=c(-0.1+borne_min.b,borne_max.b+0.1),
             type='b',xlab=expression(alpha),ylab="biais moyen")
   lines(RES0.5.3$k,RES0.5.3$mean.bernoulli,type="b",col="blue")
   lines(RES0.1.3$k,RES0.1.3$mean.bernoulli,type="b",col="red")
   abline(h=0)
-  #legend("topright",
-   #      c("0.1","0.2","0.5"),
-    #     col=c("red","black","blue"),
-     #     lty=1)
+  legend("topright",
+       c("0.1","0.2","0.5"),
+      col=c("red","black","blue"),
+      lty=1)
   plot.new()
   legend("topright",
     c("0.1","0.2","0.5"),
     col=c("red","black","blue"),
     lty=1)
   mtext("Influence de alpha", side = 3, line = -24, outer = TRUE)
+}
+
+### C'est la fonction à utiliser pour le plot
+
+fnct_compar_plt_biais.selon.k1 <- function(N, n, window_lambda, t_star, p) {
+  library(gridExtra)
+  library(ggplot2)
+  # Generate the data
+  set.seed(12345)
+  RES <- biais.selon.k(N, n, window_lambda[1], t_star, p = p)
+  RES0.1.3 <- data.frame(RES)
+  colnames(RES0.1.3) <- c("k", "mean.surv", "mean.cure", "mean.bernoulli")
   
+  set.seed(12345)
+  RES <- biais.selon.k(N, n, window_lambda[2], t_star, p = p)
+  RES0.2.3 <- data.frame(RES)
+  colnames(RES0.2.3) <- c("k", "mean.surv", "mean.cure", "mean.bernoulli")
   
-  # list_plot[[1]] <- plot_s
-  # list_plot[[2]] <- plot_c
-  # list_plot[[3]] <- plot_b
-  # 
-  # return(list_plot)
+  set.seed(12345)
+  RES <- biais.selon.k(N, n, window_lambda[3], t_star, p = p)
+  RES0.5.3 <- data.frame(RES)
+  colnames(RES0.5.3) <- c("k", "mean.surv", "mean.cure", "mean.bernoulli")
+  
+  # Get the min and max bounds of each variable to be used in the plots
+  borne_min <- min(
+    min(RES0.5.3$mean.surv),
+    min(RES0.1.3$mean.surv),
+    min(RES0.2.3$mean.surv)
+  )
+  
+  borne_max <- max(
+    max(RES0.5.3$mean.surv),
+    max(RES0.1.3$mean.surv),
+    max(RES0.2.3$mean.surv)
+  )
+  
+  borne_min.c <- min(
+    min(RES0.5.3$mean.cure),
+    min(RES0.1.3$mean.cure),
+    min(RES0.2.3$mean.cure)
+  )
+  
+  borne_max.c <- max(
+    max(RES0.5.3$mean.cure),
+    max(RES0.1.3$mean.cure),
+    max(RES0.2.3$mean.cure)
+  )
+  
+  borne_min.b <- min(
+    min(RES0.5.3$mean.bernoulli),
+    min(RES0.1.3$mean.bernoulli),
+    min(RES0.2.3$mean.bernoulli)
+  )
+  
+  borne_max.b <- max(
+    max(RES0.5.3$mean.bernoulli),
+    max(RES0.1.3$mean.bernoulli),
+    max(RES0.2.3$mean.bernoulli)
+  )
+  
+  # Plot the data
+gg1 <-  ggplot(RES0.2.3, aes(k, mean.surv)) +
+    geom_line(aes(color = "0.2")) +
+    geom_line(data = RES0.5.3, aes(k, mean.surv, color = "0.5")) +
+    geom_line(data = RES0.1.3, aes(k, mean.surv, color = "0.1")) +
+    scale_color_manual(name = expression(lambda), values = c("red", "black", "blue")) +
+    ylim(borne_min -0.1, borne_max+0.1)+
+    labs(
+      title = "Modèle de survie",
+      x = expression(alpha),
+      y = "biais moyen",
+      color = expression(alpha)+
+        theme_minimal())
+
+gg2 <-  ggplot(RES0.2.3, aes(k, mean.cure)) +
+  geom_line(aes(color = "0.2")) +
+  geom_line(data = RES0.5.3, aes(k, mean.cure, color = "0.5")) +
+  geom_line(data = RES0.1.3, aes(k, mean.cure, color = "0.1")) +
+  scale_color_manual(name = expression(lambda), values = c("red", "black", "blue")) +
+  ylim(borne_min.c -0.1, borne_max.c+0.1)+
+  labs(
+    title = "Modèle de guérison",
+    x = expression(alpha),
+    y = "biais moyen",
+    color = expression(alpha)+
+      theme_minimal())
+
+gg3 <-  ggplot(RES0.2.3, aes(k, mean.bernoulli)) +
+  geom_line(aes(color = "0.2")) +
+  geom_line(data = RES0.5.3, aes(k, mean.bernoulli, color = "0.5")) +
+  geom_line(data = RES0.1.3, aes(k, mean.bernoulli, color = "0.1")) +
+  scale_color_manual(name = expression(lambda), values = c("red", "black", "blue")) +
+  ylim(borne_min.b -0.1, borne_max.b+0.1)+
+  labs(
+    title = "Modèle de bernoulli",
+    x = expression(alpha),
+    y = "biais moyen",
+    color = expression(alpha)+
+      theme_minimal())
+      
+g <- grid.arrange(gg1, gg2, gg3, top = paste("influence de", expression(alpha)))
+return(g)
+
 }
 
 
-fnct_compar_plt_biais.selon.k(15, 10, c(0.1,0.2, 0.5), 6, 0.33)
+
+fnct_compar_plt_biais.selon.k1(15, 10, c(0.1, 0.2, 0.5), 6, 0.33)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-NSimulations.selon.n<-function(N,lambda,t_star){
-  results<- NULL
-  n<- 20
-  while (n<200)
-  {
-    vecteur_biais<-rep(NA,N)
-    biais<-  Simuler_Nfois_n_echantillons(N,n,lambda,t_star)
-    results<-as.data.frame(results,c(n,mean(biais) ))
-    n<- n+20
-  }
-  return(results)
-}
-
-
-
-set.seed(12345)
-RES<- NULL
-RES<- NSimulations.selon.n(10,0.2,3)
-RES0.2.3<-data.frame(RES)
-colnames( RES0.2.3)<- c("n","mean.bias")
-
-set.seed(12345)
-RES<- NULL
-RES<- NSimulations.selon.n(10,0.5,3)
-RES0.5.3<-data.frame(RES)
-colnames( RES0.5.3)<- c("n","mean.bias")
-
-set.seed(12345)
-RES<- NULL
-RES<- NSimulations.selon.n(10,0.1,3)
-RES0.1.3<-data.frame(RES)
-colnames( RES0.1.3)<- c("n","mean.bias")
-
-plot(RES0.2.3$n,RES0.2.3$mean.bias,title=paste("Influence of n"),
-     ylim=c(0,0.1),type='b',bty="n",xlab="nbre sujets",ylab="biais moyen")
-title("Influence de n et lambda")
-lines(RES0.5.3$n,RES0.5.3$mean.bias,type="b",col="blue")
-lines(RES0.1.3$n,RES0.1.3$mean.bias,type="b",col="red")
-legend("topright",c("0.1","0.2","0.5"),col=c("red","black","blue"),lty=1,bty="n")
 
