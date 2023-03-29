8e9b9e48a6c104645791b30512afd56178ff5d64
Nathan GORSE <gorsenath@gmail.com>
2023-03-29 22:36:28 +0200

    travail

--- estimateurs/estimateur_KM.R
+++ estimateurs/estimateur_KM.R
@@ -4,7 +4,7 @@ fonction_KM<-function(df,t_star){
   df<-df[,c("tox_time","is_observed")]
   indice_cens<-which(df$is_observed==0)
   if(length(indice_cens)==0){return(1)}
-  surv_object<-Surv(df$tox_time,event=df$is_observed)
+  surv_object<-Surv(as.numeric(df$tox_time),event=df$is_observed)
   fit <- survfit(surv_object ~1, data = df)
   # on cherche a recuperer les donnees au temps T=6
   #afin de pouvoir tracer la droite Toxicite =f(dose)
@@ -26,6 +26,7 @@ fonction_KM<-function(df,t_star){
 
 ##### TEST : + verification pour remplacer par tp.surv ####
 df<-Generation_un_ech(n=1000,lambda=0.5,p=0.33,k=1,t_star=6)
+print(table(df$is_observed))
 estim_KM<-fonction_KM(df)
 
 obj_surv<-Surv(as.numeric(df$tox_time),event=df$is_observed)
