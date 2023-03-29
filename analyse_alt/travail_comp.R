113c6c191967aaea06c8b388b21bd7d034099fbf
Nathan GORSE <gorsenath@gmail.com>
2023-03-29 11:00:28 +0200

    travail

--- analyse_alt/travail_comp.R
+++ analyse_alt/travail_comp.R
@@ -10,24 +10,19 @@ fonction_estim_comp_once<-function(p_cause1,p_cause2,n,type1,type2,t_star){
   data<-generation_comp(p_cause1 = p_cause1,p_cause2=p_cause2,t_star=t_star,nombre_obs = n,type1=type1,type2=type2)
   indices_non_obs<-which(data$status==0)
   if(length(indices_non_obs)==n){
-    sous_liste<-rep(0,3)
+    sous_liste<-list(rep(1,3))
     names(sous_liste)<-c("Survie","Bernoulli","Guerison")
-    return(list(sous_liste,sous_liste))
+    return(sous_liste)
   }
-  liste_estimateur<-list(rep(NA,2))
-  for (i in c(1,2)){
-      data_causei<-data[which(data$status==i),c("tox_time","is_observed")]
-      estimateursurv<-fonction_KM(df=data_causei,t_star)
-      estimateurbern<-fonction_Bern(df=data_causei)
-      estimateurcure<-fonction_cure(df=data_causei,t_star)
-      sous_liste<-list(estimateursurv,estimateurbern,estimateurcure)
-      names(sous_liste)<-c("Survie","Bernoulli","Guerison")
-      liste_estimateur[[i]]<-sous_liste
-  }
-  return(liste_estimateur)
+  estimateursurv<-fonction_KM(df=data,t_star)
+  estimateurbern<-fonction_Bern(df=data)
+  estimateurcure<-fonction_cure(df=data,t_star)
+  sous_liste<-list(estimateursurv,estimateurbern,estimateurcure)
+  names(sous_liste)<-c("Survie","Bernoulli","Guerison")
+  return(sous_liste)
 }
-p_cause1<-0.05
-p_cause2<-0.020
+p_cause1<-0.35
+p_cause2<-0.40
 n<-10
 type1<-"decreasing"
 type2<-"decreasing"
