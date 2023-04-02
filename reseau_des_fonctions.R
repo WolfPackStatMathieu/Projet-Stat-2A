0b7b16925a1cb3428a0d378c67afa69a1599caae
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-04-02 20:03:17 +0200

    réseaux fonctions

--- reseau_des_fonctions.R
+++ reseau_des_fonctions.R
@@ -74,39 +74,41 @@ nodes <- nodes_surv.R
 head(nodes)
 edges_simul_exp <-data.frame(from= c(1), to = c(53))
 edges_simuler_biais_un_n_ech<-data.frame(from= c(2), to = c(15,16,17,18,19))
-edges <- rbind(edges_simul_exp, edges_simuler_biais_un_n_ech)
 # edges_Calcul_estim_depuis_df<-data.frame(from= c(3), to = c())
-# edges <- rbind(edges_simul_exp, edges_Calcul_estim_depuis_df)
 edges_Simuler_biais_taillen<-data.frame(from= c(4), to = c(2))
-edges<-rbind(edges, edges_Simuler_biais_taillen)
 edges_Calcul_biais_moyen_taillen <- data.frame(from = c(5), to = c(4))
-edges<-rbind(edges, edges_Calcul_biais_moyen_taillen)
 edges_plots_scenario_1 <- data.frame(from = c(6), to = c(4))
-edges<-rbind(edges, edges_plots_scenario_1)
 edges_biais.selon.taille_echantillon <- data.frame(from = c(7), to = c(8))
-edges<-rbind(edges, edges_biais.selon.taille_echantillon)
 edges_fonction_generation_taille_mean <- data.frame(from = c(8), to = c(4))
-edges<-rbind(edges, edges_fonction_generation_taille_mean)
 edges_tp.surv <- data.frame(from = c(9), to = c(10))
-edges<-rbind(edges, edges_tp.surv)
 edges_tps.surv <- data.frame(from = c(10), to = c(11))
-edges<-rbind(edges, edges_tps.surv)
 # edges_clep <- data.frame(from = c(11), to = c())
-# edges<-rbind(edges, edges_clep)
 # edges_simul_temps_exp <- data.frame(from = c(12), to = c())
-# edges<-rbind(edges, edges_simul_temps_exp)
 # edges_simul_temp_weibull <- data.frame(from = c(13), to = c())
-# edges<-rbind(edges, edges_simul_temp_weibull)
 edges_simul_temps_alt <- data.frame(from = c(14), to = c(12))
-edges<-rbind(edges, edges_simul_temps_alt)
 # edges_simul_weibull <- data.frame(from = c(15), to = )
-# edges<-rbind(edges, edges_simul_weibull)
 edges_Generation_un_ech <- data.frame(from = c(16), to = c(15))
-edges<-rbind(edges, edges_Generation_un_ech)
 # edges_fonction_Bern <- data.frame(from = c(17), to = c())
-# edges<-rbind(edges, edges_fonction_Bern)
 edges_fonction_KM <- data.frame(from = c(18), to = c(10))
-edges<-rbind(edges, edges_fonction_KM)
+edges <- rbind(edges_simul_exp, edges_simuler_biais_un_n_ech
+               ,edges_Simuler_biais_taillen
+               ,edges_Calcul_biais_moyen_taillen
+               ,edges_plots_scenario_1
+               ,edges_biais.selon.taille_echantillon
+               ,edges_fonction_generation_taille_mean
+               ,edges_tp.surv
+               ,edges_tps.surv
+               #,edges_clep
+               #,edges_simul_temps_exp
+               #,edges_simul_temp_weibull
+               ,edges_simul_temps_alt
+               ,edges_simul_weibull
+               ,edges_Generation_un_ech
+               #,edges_fonction_Bern
+               ,edges_fonction_KM
+               )
+
+
 
 
 visNetwork(nodes, edges, height = "500px", width = "100%") %>%
