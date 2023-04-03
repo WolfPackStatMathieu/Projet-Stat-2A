63931b30a951a78909c11a4e3448691b3acbe586
Nathan GORSE <gorsenath@gmail.com>
2023-04-03 23:05:32 +0200

    influence de p

--- analyse_alt/travail_comp.R
+++ analyse_alt/travail_comp.R
@@ -6,8 +6,11 @@ source("estimateurs/estimateur_cure.R")
 source("estimateurs/estimateur_KM.R")
 library(survival)
 ########### fonction un n_echantillon#####
-fonction_estim_comp_once<-function(p_cause1,p_cause2,n,type1,type2,t_star){
+fonction_estim_comp_once<-function(p_cause1,n,type1,type2,t_star){
+  p_cause2<-(1-p_cause1)
   data<-generation_comp(p_cause1 = p_cause1,p_cause2=p_cause2,t_star=t_star,nombre_obs = n,type1=type1,type2=type2)
+  data$tox_time<-ifelse(data$status==1,t_star+1,data$tox_time)
+  data$is_observed<-ifelse(data$tox_time>t_star,0,1)
   indices_non_obs<-which(data$status==0)
   if(length(indices_non_obs)==n){
     sous_liste<-list(rep(1,3))
@@ -22,47 +25,46 @@ fonction_estim_comp_once<-function(p_cause1,p_cause2,n,type1,type2,t_star){
   return(sous_liste)
 }
 p_cause1<-0.35
-p_cause2<-0.40
 n<-10
 type1<-"decreasing"
 type2<-"decreasing"
 t_star<-6
-test_estim_comp<-fonction_estim_comp_once(p_cause1,p_cause2 = p_cause2,n=n,type1,type2,t_star=t_star)
+test_estim_comp<-fonction_estim_comp_once(p_cause1,n=n,type1,type2,t_star=t_star)
 set.seed(133)
-Simuler_estim_mult_times<-function(K,p_cause1,p_cause2,n,type1,type2,t_star){
-  result<-as.data.frame(t(cbind(sapply(rep(n,K),fonction_estim_comp_once,p_cause1=p_cause1,p_cause2=p_cause2,type1=type1,type2=type2,t_star=t_star))))
+Simuler_estim_mult_times<-function(K,p_cause1,n,type1,type2,t_star){
+  result<-as.data.frame(t(cbind(sapply(rep(n,K),fonction_estim_comp_once,p_cause1=p_cause1,type1=type1,type2=type2,t_star=t_star))))
   colnames(result)<-c("Survie","Guerison","Bernoulli")
   return(colMeans(sapply(result,as.numeric)))
 }
 #test<-Simuler_estim_mult_times(K=10,p_cause1=p_cause1,p_cause2=p_cause2,n=n,type1=type1,type2=type2,t_star=6)
-biais.selon.lambda_alt <-function(p_cause1,p_cause2,K,t_star, k,type1,type2){
+biais.selon.lambda_alt <-function(p_cause1,K,t_star, k,type1,type2){
   results <- NULL
   n <- 20
   while(n<200){
-    vec.biais <- Simuler_estim_mult_times(K=K,p_cause1=p_cause1,p_cause2=p_cause2,n=n,type1=type1,type2=type2,t_star=t_star)
-    biais_surv<-vec.biais[[1]]-p_cause1-p_cause2
-    biais.cure<-vec.biais[[2]]-p_cause1-p_cause2
-    biais.km<-vec.biais[[3]]-p_cause1-p_cause2
+    vec.biais <- Simuler_estim_mult_times(K=K,p_cause1=p_cause1,n=n,type1=type1,type2=type2,t_star=t_star)
+    biais_surv<-vec.biais[[1]]-p_cause1
+    biais.cure<-vec.biais[[2]]-p_cause1
+    biais.km<-vec.biais[[3]]-p_cause1
     results<-rbind(results,c(n,biais_surv,biais.cure,biais.km))
     n <- n+20
   }
   return(results)
 }
-fonction_compar_plotsn_lambda_alt <- function(N,t_star, vect_cause1,p_cause2, k,type1,type2) {
+fonction_compar_plotsn_lambda_alt <- function(N,t_star, vect_cause1,k,type1,type2) {
   library(gridExtra)
   library(ggplot2)
   # Generate the data
   set.seed(12345)
-  RES <- biais.selon.lambda_alt(p_cause1=vect_cause1[[1]],p_cause2=p_cause2,K=N, t_star=t_star, k,type1,type2)
+  RES <- biais.selon.lambda_alt(p_cause1=vect_cause1[[1]],K=N, t_star=t_star, k,type1,type2)
   RES0.3.3 <- data.frame(RES)
   colnames(RES0.3.3) <- c("n", "mean.surv", "mean.cure", "mean.bernoulli")
   set.seed(12345)
-  RES <- biais.selon.lambda_alt(p_cause1=vect_cause1[[2]],p_cause2=p_cause2,K=N, t_star=t_star, k,type1,type2)
+  RES <- biais.selon.lambda_alt(p_cause1=vect_cause1[[2]],K=N, t_star=t_star, k,type1,type2)
   RES0.5.3 <- data.frame(RES)
   colnames(RES0.5.3) <- c("n", "mean.surv", "mean.cure", "mean.bernoulli")
   
   set.seed(12345)
-  RES <- biais.selon.lambda_alt(p_cause1=vect_cause1[[3]],K=N,p_cause2=p_cause2, t_star=t_star, k,type1,type2)
+  RES <- biais.selon.lambda_alt(p_cause1=vect_cause1[[3]],K=N,t_star=t_star, k,type1,type2)
   RES0.7.3 <- data.frame(RES)
   colnames(RES0.7.3) <- c("n", "mean.surv", "mean.cure", "mean.bernoulli")
   
@@ -141,13 +143,12 @@ fonction_compar_plotsn_lambda_alt <- function(N,t_star, vect_cause1,p_cause2, k,
       x = "n",
       y = "biais moyen",
       color = "n" +
-        theme_minimal(),caption = sprintf("K = %s,  p2= %s, t_star=%s" ,
+        theme_minimal(),caption = sprintf("K = %s, t_star=%s" ,
                                           as.character(N),
-                                          as.character(p_cause2),
                                           as.character(t_star)))
   
   g <- grid.arrange(gg1, gg2, gg3, top = "influence de n et de p1")
   return(g)
   
 }
-test_alt<-fonction_compar_plotsn_lambda_alt(N=20, t_star=6, vect_cause1=c(0.33,0.5,0.7),p_cause2=0.4, k=1,type1="decreasing",type2="decreasing")
+test_alt<-fonction_compar_plotsn_lambda_alt(N=20, t_star=6, vect_cause1=c(0.33,0.5,0.7), k=1,type1="decreasing",type2="decreasing")
