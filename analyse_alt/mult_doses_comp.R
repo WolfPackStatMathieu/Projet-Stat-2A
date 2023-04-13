5bf7285792ea40e2b3ae2780179dd707b7704006
Nathan GORSE <gorsenath@gmail.com>
2023-04-13 22:22:56 +0200

    travail print et types

--- analyse_alt/mult_doses_comp.R
+++ analyse_alt/mult_doses_comp.R
@@ -1,9 +1,10 @@
 source("generation_echantillon/generation_ech_comp.R")
 source("estimateurs/mod_bernoulli.R")
 source("utils.R")
-function_estim_doses_comp<-function(n,probabilite_a_priori,t_star,type1,graine=133){
+function_estim_doses_comp<-function(n,probabilite_a_priori,t_star,type1,type2,graine=133){
   nb_doses<-length(probabilite_a_priori)
   require(dfcrm)
+  require(flexsurvcure)
   df<-matrix(NA,n,4)
   df<-as.data.frame(df)
   data_returns<-as.data.frame(matrix(NA,nb_doses,4))
@@ -14,7 +15,7 @@ function_estim_doses_comp<-function(n,probabilite_a_priori,t_star,type1,graine=1
     index_dosek<-which(df$dose==k)
     p<-probabilite_a_priori[k]
     n_k<-length(index_dosek)
-    df[index_dosek,]<-cbind(rep(k,n_k),generation_comp(p_cause1=p,p_cause2=1-p,t_star,nombre_obs=n_k,type1,type2=type1,graine=graine))
+    df[index_dosek,]<-cbind(rep(k,n_k),generation_comp(p_cause1=p,p_cause2=1-p,t_star,nombre_obs=n_k,type1=type1,type2=type2,graine=graine))
     df$tox_time<-ifelse(df$statut==2,t_star+1,df$tox_time)
     df$is_observed<-ifelse(df$tox_time>t_star,0,1)
     data_returns[k,"estimateur_bernoulli"]<-fonction_Bern(df[index_dosek,])
@@ -48,7 +49,7 @@ p<-0.3
 p2<-0.50
 
 prob_priori<-c(p,p2)
-test_mult_doses<-function_estim_doses_comp(n=10,probabilite_a_priori = prob_priori,t_star=6,type1 = "decreasing",graine=145)
+test_mult_doses<-function_estim_doses_comp(n=20,probabilite_a_priori = prob_priori,t_star=6,type1 = "decreasing",type2="decreasing",graine=145)
 
 p3<-0.7
 prob_priori<-c(p,p2,p3)
@@ -56,11 +57,11 @@ prob_priori<-c(p,p2,p3)
 
 
 #############MEAN####
-generation_comp_mean<-function(K,n,probabilite_a_priori,t_star,type1,graine_depart){
+generation_comp_mean<-function(K,n,probabilite_a_priori,t_star,type1,type2,graine_depart){
   graine_debut<-graine_depart+1
   graine_fin<-graine_depart+K
   ensemble_graine<-c(graine_depart,graine_fin)
-  result<-lapply(ensemble_graine,function_estim_doses_comp,n=n,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1)
+  result<-lapply(ensemble_graine,function_estim_doses_comp,n=n,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1,type2=type2)
   nb_doses<-length(prob_priori)
   matrice<-as.data.frame(matrix(NA,nb_doses,5))
   colnames(matrice)<-c("numero_dose","modele_bernoulli","modele_survie","modele_guerison","p")
@@ -77,7 +78,7 @@ generation_comp_mean<-function(K,n,probabilite_a_priori,t_star,type1,graine_depa
   return(matrice)
 }
 # test<-generation_comp_mean(K=50,n=25,probabilite_a_priori = prob_priori,t_star=6,type1 = "decreasing",graine=145)
-evol_n_par_dose<-function(results,n,i,K=K){
+evol_n_par_dose<-function(results,n,i,K=K,type1,type2){
   longueur_resultats<-c(1:length(n))
   function_intermed<-function(x,results,i){
     return(unlist(results[[x]][i,]))
@@ -116,36 +117,35 @@ evol_n_par_dose<-function(results,n,i,K=K){
           axis.title=element_text(family = "Helvetica", size=12),
           plot.title = element_text(family = "Helvetica", size = 10)) +
     ylim(borne_min, borne_max)+
-      labs(caption = sprintf("p=%s,N=%s",
+      labs(caption = sprintf("p=%s,N=%s,type1=%s,type2=%s",
                             as.character(result_final$p),
-                            as.character(K)))}
+                            as.character(K),as.character(type1),as.character(type2)))}
   
   gg <- {grid.arrange(gg1, gg2, ncol = 2, widths = c(8,8))}
   return(gg)
 }
-evol_biais_comp<-function(K,probabilite_a_priori,t_star,type1,graine_depart){
+evol_biais_comp<-function(K,probabilite_a_priori,t_star,type1,type2,graine_depart){
   debut <- 15
   fin <- 100
   pas <- 5
   n <- seq(debut,fin , pas)
-  results<-lapply(n,generation_comp_mean,K=K,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1,graine_depart=graine_depart)
-  ensemble_ggplots_par_dose<-lapply(c(1:length(probabilite_a_priori)),evol_n_par_dose,results=results,n=n,K=K)
+  results<-lapply(n,generation_comp_mean,K=K,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1,type2=type2,graine_depart=graine_depart)
+  ensemble_ggplots_par_dose<-lapply(c(1:length(probabilite_a_priori)),evol_n_par_dose,results=results,n=n,K=K,type1,type2)
   return(ensemble_ggplots_par_dose)
 }
-test_evol_biais<-evol_biais_comp(K=100,probabilite_a_priori=prob_priori,t_star=6,type1="constant",graine_depart=133)
+test_evol_biais<-evol_biais_comp(K=100,probabilite_a_priori=prob_priori,t_star=6,type1="constant",graine_depart=133,type2="constant")
 
 ################### EQM ##################"
-evol_eqm_comp<-function(K,probabilite_a_priori,t_star,type1,graine_depart){
+evol_eqm_comp<-function(K,probabilite_a_priori,t_star,type1,graine_depart,type2){
   debut <- 20
   fin <- 100
   pas <- 5
   n <- seq(debut,fin , pas)
-  results<-lapply(n,generation_comp_eqm,K=K,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1,graine_depart=graine_depart)
-  print(results)
-  ensemble_ggplots_par_dose<-lapply(c(1:length(probabilite_a_priori)),evol_n_par_dose_eqm,results=results,n=n,K=K)
+  results<-lapply(n,generation_comp_eqm,K=K,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1,graine_depart=graine_depart,type2=type2)
+  ensemble_ggplots_par_dose<-lapply(c(1:length(probabilite_a_priori)),evol_n_par_dose_eqm,results=results,n=n,K=K,type1=type1,type2=type2)
   return(ensemble_ggplots_par_dose)
 }
-evol_n_par_dose_eqm<-function(results,n,i,K=K){
+evol_n_par_dose_eqm<-function(results,n,i,K=K,type1,type2){
   longueur_resultats<-c(1:length(n))
   function_intermed<-function(x,results,i){
     return(unlist(results[[x]][i,]))
@@ -182,23 +182,24 @@ evol_n_par_dose_eqm<-function(results,n,i,K=K){
             axis.title=element_text(family = "Helvetica", size=12),
             plot.title = element_text(family = "Helvetica", size = 10)) +
       ylim(borne_min, borne_max)+
-      labs(caption = sprintf("p=%s,N=%s",
+      labs(caption = sprintf("p=%s,N=%s,type1=%s,type2=%s",
                              as.character(result_final$p),
-                             as.character(K)))}
+                             as.character(K),as.character(type1),as.character(type2)))}
   
   gg <- {grid.arrange(gg1, gg2, ncol = 2, widths = c(8,8))}
   return(gg)
 }
-generation_comp_eqm<-function(K,n,probabilite_a_priori,t_star,type1,graine_depart){
+generation_comp_eqm<-function(K,n,probabilite_a_priori,t_star,type1,graine_depart,type2){
+  require(ggplot2)
+  require(gridExtra)
   graine_ensemble<-graine_depart+c(1:K)
-  result<-lapply(graine_ensemble,function_estim_doses_comp,n=n,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1)
+  result<-lapply(graine_ensemble,function_estim_doses_comp,n=n,probabilite_a_priori=probabilite_a_priori,t_star=t_star,type1=type1,type2=type2)
   nb_doses<-length(prob_priori)
   matrice<-as.data.frame(matrix(NA,nb_doses,5))
   colnames(matrice)<-c("numero_dose","modele_bernoulli","modele_survie","modele_guerison","p")
   matrice$numero_dose<-c(1:nb_doses)
   for(j in c(1:nb_doses)){
     ensemble_obs_dosek<-t(cbind.data.frame(sapply(result,function(x,indice){return(x[indice,])},indice=j)))
-    print(ensemble_obs_dosek)
     ensemble_obs_dosek<-as.data.frame(ensemble_obs_dosek)
     ensemble_obs_dosek$estimateur_bernoulli<-as.numeric(ensemble_obs_dosek$estimateur_bernoulli)
     ensemble_obs_dosek$estimateur_guerison<-as.numeric(ensemble_obs_dosek$estimateur_guerison)
@@ -209,8 +210,8 @@ generation_comp_eqm<-function(K,n,probabilite_a_priori,t_star,type1,graine_depar
   }
   return(matrice)
 }
-
-test<-evol_eqm_comp(K=4,probabilite_a_priori=prob_priori,t_star=6,type1="decreasing",graine_depart=145)
+prob_prior1<-c(0.5,0.7)
+test<-evol_eqm_comp(K=10,probabilite_a_priori=prob_prior1,t_star=6,type1="decreasing",type2="decreasing",graine_depart=145)
 
 
 # test<-evol_eqm_comp(K=6,probabilite_a_priori=prob_priori,t_star=6,type1="constant",graine_depart=133)
