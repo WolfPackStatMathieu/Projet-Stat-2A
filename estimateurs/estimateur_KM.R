2a87018e1cd54daf6ed8b550e04941eb7a49434c
Nathan GORSE <gorsenath@gmail.com>
2023-04-23 11:33:40 +0200

    correction

--- estimateurs/estimateur_KM.R
+++ estimateurs/estimateur_KM.R
@@ -3,29 +3,15 @@ fonction_KM<-function(df,t_star){
   df<-df[,c("tox_time","is_observed")]
   surv_object<-Surv(as.numeric(df$tox_time),event=df$is_observed)
   fit <- survfit(surv_object ~1, data = df)
-  # on cherche a recuperer les donnees au temps T=6
-  #afin de pouvoir tracer la droite Toxicite =f(dose)
-  quantile <-quantile(fit)
-  quantile$quantile
-  centiles <- quantile(fit, 1:100/100)
-  cent <-centiles$quantile
-  m<-1
-  individu <- cent[m]
-  # on touche la proportion de tstar au premier NA
-  while (is.na(individu)==FALSE) {
-    individu <- cent[m]
-    m<- m+1
-  }
-  transformation <- m-1
-  estimateur_survie <- transformation / 100
-  return(1-estimateur_survie)
+  estimateur_survie<-1-tp.surv(fit,t_star)[1,][["surv"]]
+  return(estimateur_survie)
 }
 
 ##### TEST ####
 set.seed(123)
 df<-Generation_un_ech(n=10,lambda=0.5,p=0.33,k=1,t_star=6)
 str(df)
-estim_KM<-fonction_KM(df)
+estim_KM<-fonction_KM(df,t_star=6)
 estim_KM
 
 
